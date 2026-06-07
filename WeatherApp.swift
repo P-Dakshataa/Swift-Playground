1e1d8cbe5ac80f0cd39f630b68ef7a2437335a3c
Dakshataa <pdakshataa@gmail.com>
2026-06-07 15:48:21 +0530

    Add Weather App temperature exercise

---
+++ WeatherApp.swift
@@ -0,0 +1,23 @@
+/*
+Question:
+Imagine you are creating a weather app that computes daily and weekly temperatures.
+You will use constants and variables to define the temperature of the current day
+of the week and the average temperature for that week.
+*/
+
+let day = "Monday"
+let dailyTemperature = 75
+
+print("Today is \(day), Rise and shine")
+print("The temperature on \(day) is \(dailyTemperature) F")
+
+var temperature = 70
+print("The temperature on \(day) morning is \(temperature) F")
+
+temperature = 80
+print("The temperature on \(day) evening is \(temperature) F")
+
+let weeklyTemperature = 75
+temperature = weeklyTemperature
+
+print("The average temperature this week is \(temperature)°F.")
